9286513c6cbc3321984e578436b34af2e7309fc4
Ilya Sumbatyants <ilya.sumb@gmail.com>
2014-01-23 06:34:19 +0700

    'idle' complete wrapper

---
+++ src/mpd/commands/Idle.scala
@@ -0,0 +1,11 @@
+package mpd
+
+case class Idle(subsystems: List[Subsystem] = All() :: Nil) extends Command {
+  val command = "idle"
+  def raw = (command :: subsystems).mkString(" ")
+  def response = IdleResponse()
+}
+
+case class IdleResponse(subsystems: List[Subsystem] = Nil) extends Response {
+  def parse(raw: List[String]) = new IdleResponse(Subsystem.parse(raw))
+}
