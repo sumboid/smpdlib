3875e9a4d8718a93726652f7fae84339bcd8a7f0
Ilya Sumbatyants <ilya.sumb@gmail.com>
2014-01-27 23:31:46 +0700

    checkpoint

--- src/mpd/MPD.scala
+++ src/mpd/MPD.scala
@@ -4,64 +4,116 @@ import java.net._
 import java.io._
 import scala.io._
 
-trait LowLevelResponse[+A]
-case class Ok[+A](x: A) extends LowLevelResponse[A]
-case object Ack extends LowLevelResponse[Nothing]
-case object ConnectionError extends LowLevelResponse[Nothing]
+package mpdlow {
 
-class MPD(host: String, port: Int) {
-  val _host = host
-  val _port = port
-  var socket: Socket = null
-  var input: BufferedReader = null
-  var output: PrintStream = null
+  trait LowLevelResponse[+A]
 
-  connect
+  case class Ok[+A](x: A) extends LowLevelResponse[A]
+  case class Ack[+A] extends LowLevelResponse[A]
+  case class MPDVersion[+A] extends LowLevelResponse[A]
 
-  def send(q: Command): Unit = {
-    output.println(q.raw)
-    output.flush
-  }
+  case object ExternalError extends LowLevelResponse[Nothing]
+  case object InternalError extends LowLevelResponse[Nothing]
+  case object ConnectionError extends LowLevelResponse[Nothing]
+  case object UnknownHostError extends ConnectionError
+  case object IOError extends ConnectionError
 
-  def response(q: Command): Response = _response match {
-      case ConnectionError => connect; send(q); response(q)
-      case Ack => ErrorResponse()
-      case Ok(x) => q.response.parse(x)
-  }
+  case class MPDSocket(host: String, port: Int) {
+    var socket: Socket = null
+    var input:  BufferedReader = null
+    var output: PrintStream = null
 
-  def _response: LowLevelResponse[List[String]] = {
-    var lines = List[String]()
-    while(true) {
+    var queue = 0
 
-      var line: String = input.readLine
-      if(line == null) { return ConnectionError }
-      line.split(" ")(0) match {
-        case "OK"  => return Ok(lines)
-        case "ACK" => return Ack
-        case _ => lines ::= line
-      }
+    def send(q: String) = {
+      output.println(q)
+      output.flush
+      queue += 1
     }
-    Ok(lines)
-  }
 
-  def connect = {
-    if(socket != null) {
+    def recv = {
+      if (queue == 0) ExternalError
+
+      def _recv = {
+        var lines = List[String]()
+        while(true) {
+          var line: String = input.readLine
+          if(line == null) { return ConnectionError }
+
+          line.split(" ", 2) match {
+            case Array("OK") => return Ok(lines)
+            case Array("OK", message) => message.split(" ", 2) match {
+              case Array("MPD", vesrion) => return MPDVersion(version)
+              case _ => return Ok(lines)
+            }
+            case Array("ACK", message) => return Ack(message)
+            case _ => lines ::= line
+          }
+        }
+        Ok(lines)
+      }
+
+      queue -= 1
+      _recv
+    }
+
+    def disconnect = if(socket != null) {
       socket.close
       input.close
       output.close
-    }
-    
-    try {
-      socket = new Socket(InetAddress.getByName(_host), _port)
-      input = new BufferedReader(new InputStreamReader(socket.getInputStream))
-      output = new PrintStream(socket.getOutputStream)
-    } catch {
-      case e: UnknownHostException => println("Unknown host")
-      case e: IOException => println("No I/O")
-      case _: Throwable => println("Something wrong")
+
+      socket = null
+      input  = null
+      output = null
     }
 
-    { _response }
+    def connect = {
+      try {
+        socket = new Socket(InetAddress.getByName(host), port)
+        input  = new BufferedReader(new InputStreamReader(socket.getInputStream))
+        output = new PrintStream(socket.getOutputStream)
+      } catch {
+        case e: UnknownHostException => UnknownHostError
+        case e: IOException => IOError
+        case _: Throwable => ConnectionError
+      }
+
+      recv match {
+        case x: MPDVersion => x
+        case _ => InternalError
+      }
+    }
+
+    def reconnect = disconnect; connect
+  }
+}
+
+import mpdlow._
+
+class MPD(host: String, port: Int) {
+  val socket = MPDSocket(host, port)
+  var queue: List[Response] = Nil
+
+  def send(q: Command) = {
+    checkConnectionState
+    socket.send(q.raw)
+    queue :+ q.response
+  }
+
+  def checkConnectionState = {
+    socket.send("ping")
+    socket.recv match {
+      case ConnectionError => ConnectionError
+      case _ => Ok
+    }
+  }
+
+  def response = {
+    if (queue == Nil) ExternalError
+
+    _response match {
+
+    }
   }
 }
 
